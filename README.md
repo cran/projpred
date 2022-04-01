166830bc1e122ae973e909ff8cf0db1528ea5fe4
Frank Weber <fweber144@protonmail.com>
2022-04-01 10:10:05 +0000

    version 2.1.0

---
+++ README.md
@@ -0,0 +1,79 @@
+<!-- badges: start -->
+<!-- [![codecov](https://codecov.io/gh/stan-dev/projpred/branch/master/graph/badge.svg)](https://app.codecov.io/gh/stan-dev/projpred) -->
+[![CRAN_Status_Badge](https://www.r-pkg.org/badges/version/projpred?color=blue)](https://CRAN.R-project.org/package=projpred)
+<!-- badges: end -->
+
+# projpred [<img src="https://raw.githubusercontent.com/stan-dev/logos/master/logo_tm.png" align="right" width="120" alt="Stan Logo"/>](https://mc-stan.org)
+
+The **projpred** R package performs the projection predictive variable selection
+for generalized linear models as well as generalized linear and additive
+multilevel models. The package is compatible with the
+[**rstanarm**](https://mc-stan.org/rstanarm/) and
+[**brms**](https://paul-buerkner.github.io/brms/) packages, but custom reference
+models can also be used.
+
+The projection predictive variable selection is based on the ideas of Goutis and
+Robert (1998) and Dupuis and Robert (2003). The methods implemented in
+**projpred** are described in detail in Piironen et al. (2020) and Catalina et
+al. (2020). They are evaluated in comparison to many other methods in Piironen
+and Vehtari (2017). Type `citation("projpred")` in R (or see the `CITATION`
+file) for details on how to cite **projpred**.
+
+Currently, the supported response distributions (objects of class `family` in R)
+are `gaussian()`, `binomial()` (via the **brms** package, `brms::bernoulli()` is
+also supported), and `poisson()`.
+
+The [vignettes](https://mc-stan.org/projpred/articles/) (currently, there is
+only a single one) illustrate how to use the **projpred** functions in
+conjunction. Details on the **projpred** functions as well as some shorter
+examples may be found in the documentation.
+
+## Installation
+
+There are two ways for installing **projpred**: from
+[CRAN](https://CRAN.R-project.org/package=projpred) or from
+[GitHub](https://github.com/stan-dev/projpred). The GitHub version might be more
+recent than the CRAN version, but the CRAN version might be more stable.
+
+### From CRAN
+
+```r
+install.packages("projpred")
+```
+
+### From GitHub
+
+This requires the [**devtools**](https://devtools.r-lib.org/) package, so if
+necessary, the following code will also install **devtools** (from
+[CRAN](https://CRAN.R-project.org/package=devtools)):
+```r
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools")
+}
+devtools::install_github("stan-dev/projpred", build_vignettes = TRUE)
+```
+To save time, you may omit `build_vignettes = TRUE`.
+
+## References
+
+Catalina, A., Bürkner, P.-C., and Vehtari, A. (2020). Projection predictive
+inference for generalized linear and additive multilevel models.
+*arXiv:2010.06994*. URL: <https://arxiv.org/abs/2010.06994>.
+
+Dupuis, J. A. and Robert, C. P. (2003). Variable selection in qualitative models
+via an entropic explanatory power. *Journal of Statistical Planning and
+Inference*, **111**(1-2):77–94. DOI:
+[10.1016/S0378-3758(02)00286-0](https://doi.org/10.1016/S0378-3758(02)00286-0).
+
+Goutis, C. and Robert, C. P. (1998). Model choice in generalised linear models:
+A Bayesian approach via Kullback–Leibler projections. *Biometrika*,
+**85**(1):29–37.
+
+Piironen, J. and Vehtari, A. (2017). Comparison of Bayesian predictive methods
+for model selection. *Statistics and Computing*, **27**(3):711-735. DOI:
+[10.1007/s11222-016-9649-y](https://doi.org/10.1007/s11222-016-9649-y).
+
+Piironen, J., Paasiniemi, M., and Vehtari, A. (2020). Projective inference in
+high-dimensional problems: Prediction and feature selection. *Electronic Journal
+of Statistics*, **14**(1):2155-2197. DOI:
+[10.1214/20-EJS1711](https://doi.org/10.1214/20-EJS1711).
---
+++ inst/doc/projpred.R
@@ -0,0 +1,173 @@
+params <-
+list(EVAL = FALSE)
+
+## ---- SETTINGS-knitr, include=FALSE-------------------------------------------
+stopifnot(require(knitr))
+knitr::opts_chunk$set(
+  dev = "png",
+  dpi = 150,
+  fig.asp = 0.618,
+  fig.width = 5,
+  out.width = "60%",
+  fig.align = "center",
+  comment = NA,
+  eval = if (isTRUE(exists("params"))) params$EVAL else FALSE,
+  message = FALSE,
+  warning = FALSE
+)
+
+## -----------------------------------------------------------------------------
+#  data("df_gaussian", package = "projpred")
+#  dat_gauss <- data.frame(y = df_gaussian$y, df_gaussian$x)
+
+## -----------------------------------------------------------------------------
+#  library(rstanarm)
+
+## -----------------------------------------------------------------------------
+#  # Number of regression coefficients:
+#  ( D <- sum(grepl("^X", names(dat_gauss))) )
+
+## -----------------------------------------------------------------------------
+#  # Prior guess for the number of relevant (i.e., non-zero) regression
+#  # coefficients:
+#  p0 <- 5
+#  # Number of observations:
+#  N <- nrow(dat_gauss)
+#  # Hyperprior scale for tau, the global shrinkage parameter (note that for the
+#  # Gaussian family, 'rstanarm' will automatically scale this by the residual
+#  # standard deviation):
+#  tau0 <- p0 / (D - p0) * 1 / sqrt(N)
+
+## ---- cache=TRUE, cache.lazy=FALSE--------------------------------------------
+#  # Set this manually if desired:
+#  ncores <- parallel::detectCores(logical = FALSE)
+#  ### Only for technical reasons in this vignette (you can omit this when running
+#  ### the code yourself):
+#  ncores <- min(ncores, 2L)
+#  ###
+#  options(mc.cores = ncores)
+#  refm_fit <- stan_glm(
+#    y ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + X11 + X12 + X13 + X14 +
+#      X15 + X16 + X17 + X18 + X19 + X20,
+#    family = gaussian(),
+#    data = dat_gauss,
+#    prior = hs(global_scale = tau0),
+#    ### Only for the sake of speed (not recommended in general):
+#    chains = 2, iter = 500,
+#    ###
+#    seed = 2052109, QR = TRUE, refresh = 0
+#  )
+
+## -----------------------------------------------------------------------------
+#  library(projpred)
+
+## ---- results='hide', cache=TRUE, cache.lazy=FALSE----------------------------
+#  cvvs <- cv_varsel(
+#    refm_fit,
+#    ### Only for the sake of speed (not recommended in general):
+#    cv_method = "kfold",
+#    nclusters_pred = 20,
+#    ###
+#    nterms_max = 9,
+#    seed = 411183
+#  )
+
+## ---- fig.asp=1.5 * 0.618-----------------------------------------------------
+#  plot(cvvs, stats = c("elpd", "rmse"), deltas = TRUE)
+
+## -----------------------------------------------------------------------------
+#  modsize_decided <- 6
+
+## -----------------------------------------------------------------------------
+#  suggest_size(cvvs)
+
+## -----------------------------------------------------------------------------
+#  cvvs
+#  ### Alternative modifying the number of printed decimal places:
+#  # print(cvvs, digits = 2)
+#  ###
+
+## -----------------------------------------------------------------------------
+#  ( soltrms <- solution_terms(cvvs) )
+
+## -----------------------------------------------------------------------------
+#  ( soltrms_final <- head(soltrms, modsize_decided) )
+
+## ---- cache=TRUE, cache.lazy=FALSE--------------------------------------------
+#  prj <- project(
+#    refm_fit,
+#    solution_terms = soltrms_final,
+#    seed = 15705533
+#  )
+
+## -----------------------------------------------------------------------------
+#  prj_mat <- as.matrix(prj)
+
+## -----------------------------------------------------------------------------
+#  library(posterior)
+#  prj_drws <- as_draws_matrix(prj_mat)
+#  # In the following call, as.data.frame() is used only because pkgdown
+#  # versions > 1.6.1 don't print the tibble correctly.
+#  as.data.frame(summarize_draws(
+#    prj_drws,
+#    "median", "mad", function(x) quantile(x, probs = c(0.025, 0.975))
+#  ))
+
+## -----------------------------------------------------------------------------
+#  library(bayesplot)
+#  bayesplot_theme_set(ggplot2::theme_bw())
+#  mcmc_intervals(prj_mat) +
+#    ggplot2::coord_cartesian(xlim = c(-1.5, 1.6))
+
+## -----------------------------------------------------------------------------
+#  refm_mat <- as.matrix(refm_fit)
+#  mcmc_intervals(refm_mat, pars = colnames(prj_mat)) +
+#    ggplot2::coord_cartesian(xlim = c(-1.5, 1.6))
+
+## -----------------------------------------------------------------------------
+#  ( dat_gauss_new <- setNames(
+#    as.data.frame(replicate(length(soltrms_final), c(-1, 0, 1))),
+#    soltrms_final
+#  ) )
+
+## -----------------------------------------------------------------------------
+#  prj_linpred <- proj_linpred(prj, newdata = dat_gauss_new, integrated = TRUE)
+#  cbind(dat_gauss_new, linpred = as.vector(prj_linpred$pred))
+
+## -----------------------------------------------------------------------------
+#  prj_predict <- proj_predict(prj, .seed = 762805)
+#  # Using the 'bayesplot' package:
+#  ppc_dens_overlay(y = dat_gauss$y, yrep = prj_predict, alpha = 0.9, bw = "SJ")
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  data("VerbAgg", package = "lme4")
+#  refm_fit <- stan_glmer(
+#    r2 ~ btype + situ + mode + (btype + situ + mode | id),
+#    family = binomial(),
+#    data = VerbAgg,
+#    seed = 82616169, QR = TRUE, refresh = 0
+#  )
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  data("lasrosas.corn", package = "agridat")
+#  # Convert `year` to a `factor` (this could also be solved by using
+#  # `factor(year)` in the formula, but we avoid that here to put more emphasis on
+#  # the demonstration of the smooth term):
+#  lasrosas.corn$year <- as.factor(lasrosas.corn$year)
+#  refm_fit <- stan_gamm4(
+#    yield ~ year + topo + t2(nitro, bv),
+#    family = gaussian(),
+#    data = lasrosas.corn,
+#    seed = 4919670, QR = TRUE, refresh = 0
+#  )
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  data("gumpertz.pepper", package = "agridat")
+#  refm_fit <- stan_gamm4(
+#    disease ~ field + leaf + s(water),
+#    random = ~ (1 | row) + (1 | quadrat),
+#    family = binomial(),
+#    data = gumpertz.pepper,
+#    seed = 14209013, QR = TRUE, refresh = 0
+#  )
+
